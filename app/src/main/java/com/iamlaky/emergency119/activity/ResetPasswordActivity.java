729bedea7f5ce0f854b77eb456705bf90ed9dee9
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-08 18:08:09 +0530

    init

---
+++ app/src/main/java/com/iamlaky/emergency119/activity/ResetPasswordActivity.java
@@ -0,0 +1,20 @@
+package com.iamlaky.emergency119.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.iamlaky.emergency119.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
