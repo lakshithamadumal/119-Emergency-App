fe6dd998ad689a10f0cad1e1ce15d219e0eae945
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-09 00:02:53 +0530

    ඉන්ට්

---
+++ app/src/main/java/com/iamlaky/emergency119/activity/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.iamlaky.emergency119.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.iamlaky.emergency119.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
