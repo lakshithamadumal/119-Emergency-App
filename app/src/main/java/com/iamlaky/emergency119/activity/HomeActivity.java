912d7c4eef1c44fd06d55cc9803e2321bdce4a95
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-09 23:08:36 +0530

    effect

--- app/src/main/java/com/iamlaky/emergency119/activity/HomeActivity.java
+++ app/src/main/java/com/iamlaky/emergency119/activity/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.iamlaky.emergency119.activity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +15,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View pulseView = findViewById(R.id.pulseView);
+
+// රවුම ලොකු වෙලා මැකී යන Animation එක
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(pulseView, "scaleX", 1.0f, 2.0f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(pulseView, "scaleY", 1.0f, 2.0f);
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(pulseView, "alpha", 0.5f, 0f);
+
+        scaleX.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleY.setRepeatCount(ObjectAnimator.INFINITE);
+        alpha.setRepeatCount(ObjectAnimator.INFINITE);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(scaleX, scaleY, alpha);
+        animatorSet.setDuration(1500); // වේගය මෙතනින් වෙනස් කරන්න පුළුවන්
+        animatorSet.start();
 
     }
 }
