f2033a3ddbc0a2c3854d12c9c668dc10c3b584f4
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-15 16:10:14 +0530

    iniy

--- app/src/main/java/com/iamlaky/emergency119/activity/MapActivity.java
+++ app/src/main/java/com/iamlaky/emergency119/activity/MapActivity.java
@@ -1,26 +1,35 @@
 package com.iamlaky.emergency119.activity;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 import com.iamlaky.emergency119.R;
 
 public class MapActivity extends AppCompatActivity {
 
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_map);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mapView = findViewById(R.id.mapView);
+
+        mapView.getMapboxMap().loadStyleUri(
+                Style.MAPBOX_STREETS,
+                style -> {
+                    CameraOptions cameraOptions = new CameraOptions.Builder()
+                            .center(Point.fromLngLat(79.8612, 6.9271))
+                            .zoom(15.0)
+                            .build();
+
+                    mapView.getMapboxMap().setCamera(cameraOptions);
+                }
+        );
     }
 }
