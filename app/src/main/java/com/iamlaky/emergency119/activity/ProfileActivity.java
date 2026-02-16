5947669ca430d8e8c01782f67a6d95e59c19ab1d
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-16 23:11:10 +0530

    ඉන්ට්

--- app/src/main/java/com/iamlaky/emergency119/activity/ProfileActivity.java
+++ app/src/main/java/com/iamlaky/emergency119/activity/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.iamlaky.emergency119.activity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,6 +22,9 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        View pulseView = findViewById(R.id.pulseView);
+        View pulseCallView = findViewById(R.id.pulseCallView);
+
         // 1. Full Name Input එක හදන හැටි
         View nameInput = findViewById(R.id.inputFullName);
         ((TextView) nameInput.findViewById(R.id.tvInputLabel)).setText("Full name");
@@ -39,5 +44,20 @@ public class ProfileActivity extends AppCompatActivity {
         View nicInput = findViewById(R.id.inputNIC);
         ((TextView) nicInput.findViewById(R.id.tvInputLabel)).setText("NIC / Passport Number");
         ((EditText) nicInput.findViewById(R.id.etInputValue)).setHint("200330800692");
+
+        // Call Button Animation
+        ObjectAnimator cScaleX = ObjectAnimator.ofFloat(pulseCallView, "scaleX", 1.0f, 1.4f);
+        ObjectAnimator cScaleY = ObjectAnimator.ofFloat(pulseCallView, "scaleY", 1.0f, 1.4f);
+        ObjectAnimator cAlpha = ObjectAnimator.ofFloat(pulseCallView, "alpha", 1.0f, 0f);
+
+        cScaleX.setRepeatCount(ObjectAnimator.INFINITE);
+        cScaleY.setRepeatCount(ObjectAnimator.INFINITE);
+        cAlpha.setRepeatCount(ObjectAnimator.INFINITE);
+
+        AnimatorSet callPulseSet = new AnimatorSet();
+        callPulseSet.playTogether(cScaleX, cScaleY, cAlpha);
+        callPulseSet.setDuration(2000);
+        callPulseSet.start();
+        // Call Button Animation
     }
 }
